f0951a0c359e72cc7763b58cefba148902aa368d
Mario P. <me@marioparaschiv.com>
2026-07-04 00:38:33 +0200

    feat(ui): add addon card and card-based list

---
+++ packages/client/src/ui/addons/index.ts
@@ -0,0 +1,6 @@
+export { default as AddonCardHeader } from './addon-card-header';
+export { default as AddonOverflow } from './addon-overflow';
+export { default as AddonSwitch } from './addon-switch';
+export { default as AddonRadio } from './addon-radio';
+export { default as AddonCard } from './addon-card';
+export { default as AddonList } from './addon-list';
